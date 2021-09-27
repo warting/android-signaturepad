0590ecf077b879bf6ed670370ec00eaec3712287
warting <stefan@warting.se>
2021-09-27 22:30:50 +0200

    convert java to kotlin

--- app/src/main/java/se/warting/signaturepad/DataBindingActivity.kt
+++ app/src/main/java/se/warting/signaturepad/DataBindingActivity.kt
@@ -21,194 +21,202 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+package se.warting.signaturepad
 
-package se.warting.signaturepad;
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.databinding.DataBindingUtil
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.io.OutputStreamWriter
+import se.warting.signaturepad.databinding.ActivityDatabindBinding
+import se.warting.signatureview.views.SignedListener
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
-import androidx.databinding.DataBindingUtil;
-
-import se.warting.signatureview.views.SignaturePad;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-
-import se.warting.signaturepad.databinding.ActivityDatabindBinding;
-import se.warting.signatureview.views.SignedListener;
-
-
-public class DataBindingActivity extends Activity {
-
-    private static final int REQUEST_EXTERNAL_STORAGE = 1;
-    private static String[] PERMISSIONS_STORAGE = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
-
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        verifyStoragePermissions(this);
+class DataBindingActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        verifyStoragePermissions(this)
 
         // The layout for this activity is a Data Binding layout so it needs to be inflated using
         // DataBindingUtil.
-        ActivityDatabindBinding binding = DataBindingUtil.setContentView(
-                this, R.layout.activity_databind);
-
-        SignedListener onStartSigning = new SignedListener() {
-            @Override
-            public void onStartSigning() {
-                Toast.makeText(DataBindingActivity.this, "OnStartSigning", Toast.LENGTH_SHORT).show();
+        val binding: ActivityDatabindBinding = DataBindingUtil.setContentView(
+            this, R.layout.activity_databind
+        )
+        val onStartSigning: SignedListener = object : SignedListener {
+            override fun onStartSigning() {
+                Toast.makeText(this@DataBindingActivity, "OnStartSigning", Toast.LENGTH_SHORT)
+                    .show()
             }
 
-            @Override
-            public void onSigned() {
-                binding.saveButton.setEnabled(true);
-                binding.clearButton.setEnabled(true);
+            override fun onSigned() {
+                binding.saveButton.isEnabled = true
+                binding.clearButton.isEnabled = true
             }
 
-            @Override
-            public void onClear() {
-                binding.saveButton.setEnabled(false);
-                binding.clearButton.setEnabled(false);
+            override fun onClear() {
+                binding.saveButton.isEnabled = false
+                binding.clearButton.isEnabled = false
             }
-        };
-
-
-        binding.setBinding(onStartSigning);
-
-        binding.clearButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                binding.signaturePad.clear();
+        }
+        binding.binding = onStartSigning
+        binding.clearButton.setOnClickListener { binding.signaturePad.clear() }
+        binding.saveButton.setOnClickListener {
+            val signatureBitmap = binding.signaturePad.signatureBitmap
+            if (addJpgSignatureToGallery(signatureBitmap)) {
+                Toast.makeText(
+                    this@DataBindingActivity,
+                    "Signature saved into the Gallery",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(
+                    this@DataBindingActivity,
+                    "Unable to store the signature",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
-        });
-
-        binding.saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Bitmap signatureBitmap = binding.signaturePad.getSignatureBitmap();
-                if (addJpgSignatureToGallery(signatureBitmap)) {
-                    Toast.makeText(DataBindingActivity.this, "Signature saved into the Gallery", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(DataBindingActivity.this, "Unable to store the signature", Toast.LENGTH_SHORT).show();
-                }
-                if (addSvgSignatureToGallery(binding.signaturePad.getSignatureSvg())) {
-                    Toast.makeText(DataBindingActivity.this, "SVG Signature saved into the Gallery", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(DataBindingActivity.this, "Unable to store the SVG signature", Toast.LENGTH_SHORT).show();
-                }
+            if (addSvgSignatureToGallery(binding.signaturePad.signatureSvg)) {
+                Toast.makeText(
+                    this@DataBindingActivity,
+                    "SVG Signature saved into the Gallery",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(
+                    this@DataBindingActivity,
+                    "Unable to store the SVG signature",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
-        });
+        }
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String permissions[], @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_EXTERNAL_STORAGE: {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_EXTERNAL_STORAGE -> {
+
                 // If request is cancelled, the result arrays are empty.
-                if (grantResults.length <= 0
-                        || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(DataBindingActivity.this, "Cannot write images to external storage", Toast.LENGTH_SHORT).show();
+                if (grantResults.size <= 0 ||
+                    grantResults[0] != PackageManager.PERMISSION_GRANTED
+                ) {
+                    Toast.makeText(
+                        this@DataBindingActivity,
+                        "Cannot write images to external storage",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
         }
     }
 
-    @NonNull
-    public File getAlbumStorageDir(@NonNull String albumName) {
+    fun getAlbumStorageDir(albumName: String): File {
         // Get the directory for the user's public pictures directory.
-        File file = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), albumName);
+        val file = File(
+            Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES
+            ), albumName
+        )
         if (!file.mkdirs()) {
-            Log.e("SignaturePad", "Directory not created");
+            Log.e("SignaturePad", "Directory not created")
         }
-        return file;
+        return file
     }
 
-    public void saveBitmapToJPG(@NonNull Bitmap bitmap, @NonNull File photo) throws IOException {
-        Bitmap newBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(newBitmap);
-        canvas.drawColor(Color.WHITE);
-        canvas.drawBitmap(bitmap, 0, 0, null);
-        OutputStream stream = new FileOutputStream(photo);
-        newBitmap.compress(Bitmap.CompressFormat.JPEG, 80, stream);
-        stream.close();
+    @SuppressWarnings("MagicNumber")
+    @Throws(IOException::class)
+    fun saveBitmapToJPG(bitmap: Bitmap, photo: File) {
+        val newBitmap = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(newBitmap)
+        canvas.drawColor(Color.WHITE)
+        canvas.drawBitmap(bitmap, 0f, 0f, null)
+        val stream: OutputStream = FileOutputStream(photo)
+        newBitmap.compress(Bitmap.CompressFormat.JPEG, 80, stream)
+        stream.close()
     }
 
-    public boolean addJpgSignatureToGallery(@NonNull Bitmap signature) {
-        boolean result = false;
-        try {
-            File photo = new File(getAlbumStorageDir("SignaturePad"), "Signature_" + System.currentTimeMillis() + ".jpg");
-            saveBitmapToJPG(signature, photo);
-            scanMediaFile(photo);
-            result = true;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return result;
+    private fun addJpgSignatureToGallery(signature: Bitmap): Boolean {
+        var result = false
+
+            val photo = File(
+                getAlbumStorageDir("SignaturePad"),
+                "Signature_" + System.currentTimeMillis() + ".jpg"
+            )
+            saveBitmapToJPG(signature, photo)
+            scanMediaFile(photo)
+            result = true
+
+        return result
     }
 
-    private void scanMediaFile(File photo) {
-        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        Uri contentUri = Uri.fromFile(photo);
-        mediaScanIntent.setData(contentUri);
-        DataBindingActivity.this.sendBroadcast(mediaScanIntent);
+    private fun scanMediaFile(photo: File) {
+        val mediaScanIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
+        val contentUri = Uri.fromFile(photo)
+        mediaScanIntent.data = contentUri
+        this@DataBindingActivity.sendBroadcast(mediaScanIntent)
     }
 
-    public boolean addSvgSignatureToGallery(@NonNull String signatureSvg) {
-        boolean result = false;
-        try {
-            File svgFile = new File(getAlbumStorageDir("SignaturePad"), "Signature_" + System.currentTimeMillis() + ".svg");
-            OutputStream stream = new FileOutputStream(svgFile);
-            OutputStreamWriter writer = new OutputStreamWriter(stream);
-            writer.write(signatureSvg);
-            writer.close();
-            stream.flush();
-            stream.close();
-            scanMediaFile(svgFile);
-            result = true;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return result;
+    private fun addSvgSignatureToGallery(signatureSvg: String): Boolean {
+        var result = false
+
+            val svgFile = File(
+                getAlbumStorageDir("SignaturePad"),
+                "Signature_" + System.currentTimeMillis() + ".svg"
+            )
+            val stream: OutputStream = FileOutputStream(svgFile)
+            val writer = OutputStreamWriter(stream)
+            writer.write(signatureSvg)
+            writer.close()
+            stream.flush()
+            stream.close()
+            scanMediaFile(svgFile)
+            result = true
+
+        return result
     }
 
-    /**
-     * Checks if the app has permission to write to device storage
-     * <p/>
-     * If the app does not has permission then the user will be prompted to grant permissions
-     *
-     * @param activity the activity from which permissions are checked
-     */
-    public static void verifyStoragePermissions(@NonNull Activity activity) {
-        // Check if we have write permission
-        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+    companion object {
+        private const val REQUEST_EXTERNAL_STORAGE = 1
+        private val PERMISSIONS_STORAGE = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            // We don't have permission so prompt the user
-            ActivityCompat.requestPermissions(
+        /**
+         * Checks if the app has permission to write to device storage
+         *
+         *
+         * If the app does not has permission then the user will be prompted to grant permissions
+         *
+         * @param activity the activity from which permissions are checked
+         */
+        fun verifyStoragePermissions(activity: Activity) {
+            // Check if we have write permission
+            val permission = ActivityCompat.checkSelfPermission(
+                activity,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                // We don't have permission so prompt the user
+                ActivityCompat.requestPermissions(
                     activity,
                     PERMISSIONS_STORAGE,
                     REQUEST_EXTERNAL_STORAGE
-            );
+                )
+            }
         }
     }
 }
--- app/src/main/java/se/warting/signaturepad/ViewActivity.kt
+++ app/src/main/java/se/warting/signaturepad/ViewActivity.kt
@@ -21,188 +21,199 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+package se.warting.signaturepad
 
-package se.warting.signaturepad;
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.io.OutputStreamWriter
+import se.warting.signatureview.views.SignaturePad
+import se.warting.signatureview.views.SignedListener
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
-
-import se.warting.signatureview.views.SignaturePad;
-import se.warting.signatureview.views.SignedListener;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-
-public class ViewActivity extends Activity {
-
-    private static final int REQUEST_EXTERNAL_STORAGE = 1;
-    private static String[] PERMISSIONS_STORAGE = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
-
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        verifyStoragePermissions(this);
-        setContentView(R.layout.activity_view);
-
-        Button mSaveButton = (Button) findViewById(R.id.save_button);
-        Button mClearButton = (Button) findViewById(R.id.clear_button);
-        SignaturePad mSignaturePad = (SignaturePad) findViewById(R.id.signature_pad);
-        mSignaturePad.setOnSignedListener(new SignedListener() {
-            @Override
-            public void onStartSigning() {
-                Toast.makeText(ViewActivity.this, "OnStartSigning", Toast.LENGTH_SHORT).show();
+class ViewActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        verifyStoragePermissions(this)
+        setContentView(R.layout.activity_view)
+        val mSaveButton = findViewById<View>(R.id.save_button) as Button
+        val mClearButton = findViewById<View>(R.id.clear_button) as Button
+        val mSignaturePad = findViewById<View>(R.id.signature_pad) as SignaturePad
+        mSignaturePad.setOnSignedListener(object : SignedListener {
+            override fun onStartSigning() {
+                Toast.makeText(this@ViewActivity, "OnStartSigning", Toast.LENGTH_SHORT).show()
             }
 
-            @Override
-            public void onSigned() {
-                mSaveButton.setEnabled(true);
-                mClearButton.setEnabled(true);
+            override fun onSigned() {
+                mSaveButton.isEnabled = true
+                mClearButton.isEnabled = true
             }
 
-            @Override
-            public void onClear() {
-                mSaveButton.setEnabled(false);
-                mClearButton.setEnabled(false);
+            override fun onClear() {
+                mSaveButton.isEnabled = false
+                mClearButton.isEnabled = false
             }
-        });
-
-
-        mClearButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mSignaturePad.clear();
+        })
+        mClearButton.setOnClickListener { mSignaturePad.clear() }
+        mSaveButton.setOnClickListener {
+            val signatureBitmap = mSignaturePad.signatureBitmap
+            if (addJpgSignatureToGallery(signatureBitmap)) {
+                Toast.makeText(
+                    this@ViewActivity,
+                    "Signature saved into the Gallery",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(
+                    this@ViewActivity,
+                    "Unable to store the signature",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
-        });
-
-        mSaveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Bitmap signatureBitmap = mSignaturePad.getSignatureBitmap();
-                if (addJpgSignatureToGallery(signatureBitmap)) {
-                    Toast.makeText(ViewActivity.this, "Signature saved into the Gallery", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(ViewActivity.this, "Unable to store the signature", Toast.LENGTH_SHORT).show();
-                }
-                if (addSvgSignatureToGallery(mSignaturePad.getSignatureSvg())) {
-                    Toast.makeText(ViewActivity.this, "SVG Signature saved into the Gallery", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(ViewActivity.this, "Unable to store the SVG signature", Toast.LENGTH_SHORT).show();
-                }
+            if (addSvgSignatureToGallery(mSignaturePad.signatureSvg)) {
+                Toast.makeText(
+                    this@ViewActivity,
+                    "SVG Signature saved into the Gallery",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(
+                    this@ViewActivity,
+                    "Unable to store the SVG signature",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
-        });
+        }
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String permissions[], @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_EXTERNAL_STORAGE: {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_EXTERNAL_STORAGE -> {
+
                 // If request is cancelled, the result arrays are empty.
-                if (grantResults.length <= 0
-                        || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(ViewActivity.this, "Cannot write images to external storage", Toast.LENGTH_SHORT).show();
+                if (grantResults.size <= 0 ||
+                    grantResults[0] != PackageManager.PERMISSION_GRANTED
+                ) {
+                    Toast.makeText(
+                        this@ViewActivity,
+                        "Cannot write images to external storage",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
         }
     }
 
-    @NonNull
-    public File getAlbumStorageDir(@NonNull String albumName) {
+    fun getAlbumStorageDir(albumName: String): File {
         // Get the directory for the user's public pictures directory.
-        File file = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), albumName);
+        val file = File(
+            Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES
+            ), albumName
+        )
         if (!file.mkdirs()) {
-            Log.e("SignaturePad", "Directory not created");
+            Log.e("SignaturePad", "Directory not created")
         }
-        return file;
+        return file
     }
 
-    public void saveBitmapToJPG(@NonNull Bitmap bitmap, @NonNull File photo) throws IOException {
-        Bitmap newBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(newBitmap);
-        canvas.drawColor(Color.WHITE);
-        canvas.drawBitmap(bitmap, 0, 0, null);
-        OutputStream stream = new FileOutputStream(photo);
-        newBitmap.compress(Bitmap.CompressFormat.JPEG, 80, stream);
-        stream.close();
+    @SuppressWarnings("MagicNumber")
+    @Throws(IOException::class)
+    fun saveBitmapToJPG(bitmap: Bitmap, photo: File) {
+        val newBitmap = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(newBitmap)
+        canvas.drawColor(Color.WHITE)
+        canvas.drawBitmap(bitmap, 0f, 0f, null)
+        val stream: OutputStream = FileOutputStream(photo)
+        newBitmap.compress(Bitmap.CompressFormat.JPEG, 80, stream)
+        stream.close()
     }
 
-    public boolean addJpgSignatureToGallery(@NonNull Bitmap signature) {
-        boolean result = false;
-        try {
-            File photo = new File(getAlbumStorageDir("SignaturePad"), "Signature_" + System.currentTimeMillis() + ".jpg");
-            saveBitmapToJPG(signature, photo);
-            scanMediaFile(photo);
-            result = true;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return result;
+    private fun addJpgSignatureToGallery(signature: Bitmap): Boolean {
+        var result = false
+
+        val photo = File(
+            getAlbumStorageDir("SignaturePad"),
+            "Signature_" + System.currentTimeMillis() + ".jpg"
+        )
+        saveBitmapToJPG(signature, photo)
+        scanMediaFile(photo)
+        result = true
+
+        return result
     }
 
-    private void scanMediaFile(File photo) {
-        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        Uri contentUri = Uri.fromFile(photo);
-        mediaScanIntent.setData(contentUri);
-        ViewActivity.this.sendBroadcast(mediaScanIntent);
+    private fun scanMediaFile(photo: File) {
+        val mediaScanIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
+        val contentUri = Uri.fromFile(photo)
+        mediaScanIntent.data = contentUri
+        this@ViewActivity.sendBroadcast(mediaScanIntent)
     }
 
-    public boolean addSvgSignatureToGallery(@NonNull String signatureSvg) {
-        boolean result = false;
-        try {
-            File svgFile = new File(getAlbumStorageDir("SignaturePad"), "Signature_" + System.currentTimeMillis() + ".svg");
-            OutputStream stream = new FileOutputStream(svgFile);
-            OutputStreamWriter writer = new OutputStreamWriter(stream);
-            writer.write(signatureSvg);
-            writer.close();
-            stream.flush();
-            stream.close();
-            scanMediaFile(svgFile);
-            result = true;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return result;
+    private fun addSvgSignatureToGallery(signatureSvg: String): Boolean {
+        var result = false
+
+        val svgFile = File(
+            getAlbumStorageDir("SignaturePad"),
+            "Signature_" + System.currentTimeMillis() + ".svg"
+        )
+        val stream: OutputStream = FileOutputStream(svgFile)
+        val writer = OutputStreamWriter(stream)
+        writer.write(signatureSvg)
+        writer.close()
+        stream.flush()
+        stream.close()
+        scanMediaFile(svgFile)
+        result = true
+
+        return result
     }
 
-    /**
-     * Checks if the app has permission to write to device storage
-     * <p/>
-     * If the app does not has permission then the user will be prompted to grant permissions
-     *
-     * @param activity the activity from which permissions are checked
-     */
-    public static void verifyStoragePermissions(@NonNull Activity activity) {
-        // Check if we have write permission
-        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+    companion object {
+        private const val REQUEST_EXTERNAL_STORAGE = 1
+        private val PERMISSIONS_STORAGE = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            // We don't have permission so prompt the user
-            ActivityCompat.requestPermissions(
+        /**
+         * Checks if the app has permission to write to device storage
+         *
+         *
+         * If the app does not has permission then the user will be prompted to grant permissions
+         *
+         * @param activity the activity from which permissions are checked
+         */
+        fun verifyStoragePermissions(activity: Activity) {
+            // Check if we have write permission
+            val permission = ActivityCompat.checkSelfPermission(
+                activity,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                // We don't have permission so prompt the user
+                ActivityCompat.requestPermissions(
                     activity,
                     PERMISSIONS_STORAGE,
                     REQUEST_EXTERNAL_STORAGE
-            );
+                )
+            }
         }
     }
 }
