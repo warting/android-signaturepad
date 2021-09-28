61c3bfc2385ea69856a0003174b966747778b821
warting <stefan@warting.se>
2021-09-28 09:24:55 +0200

    Add compose callbacks onSigned / onClear / onStartSigning

--- app/src/main/java/se/warting/signaturepad/DataBindingActivity.kt
+++ app/src/main/java/se/warting/signaturepad/DataBindingActivity.kt
@@ -23,32 +23,17 @@
  */
 package se.warting.signaturepad
 
-import android.Manifest
 import android.app.Activity
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Color
-import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
 import android.util.Log
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
-import java.io.OutputStream
-import java.io.OutputStreamWriter
 import se.warting.signaturepad.databinding.ActivityDatabindBinding
 import se.warting.signatureview.views.SignedListener
 
 class DataBindingActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        verifyStoragePermissions(this)
 
         // The layout for this activity is a Data Binding layout so it needs to be inflated using
         // DataBindingUtil.
@@ -75,147 +60,16 @@ class DataBindingActivity : Activity() {
         binding.clearButton.setOnClickListener { binding.signaturePad.clear() }
         binding.saveButton.setOnClickListener {
             val signatureBitmap = binding.signaturePad.signatureBitmap
-            if (addJpgSignatureToGallery(signatureBitmap)) {
-                Toast.makeText(
-                    this@DataBindingActivity,
-                    "Signature saved into the Gallery",
-                    Toast.LENGTH_SHORT
-                ).show()
-            } else {
-                Toast.makeText(
-                    this@DataBindingActivity,
-                    "Unable to store the signature",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-            if (addSvgSignatureToGallery(binding.signaturePad.signatureSvg)) {
-                Toast.makeText(
-                    this@DataBindingActivity,
-                    "SVG Signature saved into the Gallery",
-                    Toast.LENGTH_SHORT
-                ).show()
-            } else {
-                Toast.makeText(
-                    this@DataBindingActivity,
-                    "Unable to store the SVG signature",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        }
-    }
+            val signatureSvg = binding.signaturePad.signatureSvg
+            val transparentSignatureBitmap = binding.signaturePad.transparentSignatureBitmap
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        when (requestCode) {
-            REQUEST_EXTERNAL_STORAGE -> {
-
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.size <= 0 ||
-                    grantResults[0] != PackageManager.PERMISSION_GRANTED
-                ) {
-                    Toast.makeText(
-                        this@DataBindingActivity,
-                        "Cannot write images to external storage",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
-        }
-    }
-
-    fun getAlbumStorageDir(albumName: String): File {
-        // Get the directory for the user's public pictures directory.
-        val file = File(
-            Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES
-            ), albumName
-        )
-        if (!file.mkdirs()) {
-            Log.e("SignaturePad", "Directory not created")
-        }
-        return file
-    }
-
-    @SuppressWarnings("MagicNumber")
-    @Throws(IOException::class)
-    fun saveBitmapToJPG(bitmap: Bitmap, photo: File) {
-        val newBitmap = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
-        val canvas = Canvas(newBitmap)
-        canvas.drawColor(Color.WHITE)
-        canvas.drawBitmap(bitmap, 0f, 0f, null)
-        val stream: OutputStream = FileOutputStream(photo)
-        newBitmap.compress(Bitmap.CompressFormat.JPEG, 80, stream)
-        stream.close()
-    }
-
-    private fun addJpgSignatureToGallery(signature: Bitmap): Boolean {
-        var result = false
-
-            val photo = File(
-                getAlbumStorageDir("SignaturePad"),
-                "Signature_" + System.currentTimeMillis() + ".jpg"
-            )
-            saveBitmapToJPG(signature, photo)
-            scanMediaFile(photo)
-            result = true
-
-        return result
-    }
-
-    private fun scanMediaFile(photo: File) {
-        val mediaScanIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
-        val contentUri = Uri.fromFile(photo)
-        mediaScanIntent.data = contentUri
-        this@DataBindingActivity.sendBroadcast(mediaScanIntent)
-    }
-
-    private fun addSvgSignatureToGallery(signatureSvg: String): Boolean {
-        var result = false
-
-            val svgFile = File(
-                getAlbumStorageDir("SignaturePad"),
-                "Signature_" + System.currentTimeMillis() + ".svg"
-            )
-            val stream: OutputStream = FileOutputStream(svgFile)
-            val writer = OutputStreamWriter(stream)
-            writer.write(signatureSvg)
-            writer.close()
-            stream.flush()
-            stream.close()
-            scanMediaFile(svgFile)
-            result = true
-
-        return result
-    }
-
-    companion object {
-        private const val REQUEST_EXTERNAL_STORAGE = 1
-        private val PERMISSIONS_STORAGE = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-
-        /**
-         * Checks if the app has permission to write to device storage
-         *
-         *
-         * If the app does not has permission then the user will be prompted to grant permissions
-         *
-         * @param activity the activity from which permissions are checked
-         */
-        fun verifyStoragePermissions(activity: Activity) {
-            // Check if we have write permission
-            val permission = ActivityCompat.checkSelfPermission(
-                activity,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-            )
-            if (permission != PackageManager.PERMISSION_GRANTED) {
-                // We don't have permission so prompt the user
-                ActivityCompat.requestPermissions(
-                    activity,
-                    PERMISSIONS_STORAGE,
-                    REQUEST_EXTERNAL_STORAGE
+            if (BuildConfig.DEBUG) {
+                Log.d("DataBindingActivity", "Bitmap size: " + signatureBitmap.byteCount)
+                Log.d(
+                    "DataBindingActivity",
+                    "Bitmap trasparent size: " + transparentSignatureBitmap.byteCount
                 )
+                Log.d("DataBindingActivity", "Svg length: " + signatureSvg.length)
             }
         }
     }
--- app/src/main/java/se/warting/signaturepad/ViewActivity.kt
+++ app/src/main/java/se/warting/signaturepad/ViewActivity.kt
@@ -23,33 +23,19 @@
  */
 package se.warting.signaturepad
 
-import android.Manifest
 import android.app.Activity
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Color
-import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
-import java.io.OutputStream
-import java.io.OutputStreamWriter
 import se.warting.signatureview.views.SignaturePad
 import se.warting.signatureview.views.SignedListener
 
 class ViewActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        verifyStoragePermissions(this)
+
         setContentView(R.layout.activity_view)
         val mSaveButton = findViewById<View>(R.id.save_button) as Button
         val mClearButton = findViewById<View>(R.id.clear_button) as Button
@@ -72,147 +58,15 @@ class ViewActivity : Activity() {
         mClearButton.setOnClickListener { mSignaturePad.clear() }
         mSaveButton.setOnClickListener {
             val signatureBitmap = mSignaturePad.signatureBitmap
-            if (addJpgSignatureToGallery(signatureBitmap)) {
-                Toast.makeText(
-                    this@ViewActivity,
-                    "Signature saved into the Gallery",
-                    Toast.LENGTH_SHORT
-                ).show()
-            } else {
-                Toast.makeText(
-                    this@ViewActivity,
-                    "Unable to store the signature",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-            if (addSvgSignatureToGallery(mSignaturePad.signatureSvg)) {
-                Toast.makeText(
-                    this@ViewActivity,
-                    "SVG Signature saved into the Gallery",
-                    Toast.LENGTH_SHORT
-                ).show()
-            } else {
-                Toast.makeText(
-                    this@ViewActivity,
-                    "Unable to store the SVG signature",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        when (requestCode) {
-            REQUEST_EXTERNAL_STORAGE -> {
-
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.size <= 0 ||
-                    grantResults[0] != PackageManager.PERMISSION_GRANTED
-                ) {
-                    Toast.makeText(
-                        this@ViewActivity,
-                        "Cannot write images to external storage",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
-        }
-    }
-
-    fun getAlbumStorageDir(albumName: String): File {
-        // Get the directory for the user's public pictures directory.
-        val file = File(
-            Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES
-            ), albumName
-        )
-        if (!file.mkdirs()) {
-            Log.e("SignaturePad", "Directory not created")
-        }
-        return file
-    }
-
-    @SuppressWarnings("MagicNumber")
-    @Throws(IOException::class)
-    fun saveBitmapToJPG(bitmap: Bitmap, photo: File) {
-        val newBitmap = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
-        val canvas = Canvas(newBitmap)
-        canvas.drawColor(Color.WHITE)
-        canvas.drawBitmap(bitmap, 0f, 0f, null)
-        val stream: OutputStream = FileOutputStream(photo)
-        newBitmap.compress(Bitmap.CompressFormat.JPEG, 80, stream)
-        stream.close()
-    }
-
-    private fun addJpgSignatureToGallery(signature: Bitmap): Boolean {
-        var result = false
-
-        val photo = File(
-            getAlbumStorageDir("SignaturePad"),
-            "Signature_" + System.currentTimeMillis() + ".jpg"
-        )
-        saveBitmapToJPG(signature, photo)
-        scanMediaFile(photo)
-        result = true
-
-        return result
-    }
-
-    private fun scanMediaFile(photo: File) {
-        val mediaScanIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
-        val contentUri = Uri.fromFile(photo)
-        mediaScanIntent.data = contentUri
-        this@ViewActivity.sendBroadcast(mediaScanIntent)
-    }
-
-    private fun addSvgSignatureToGallery(signatureSvg: String): Boolean {
-        var result = false
-
-        val svgFile = File(
-            getAlbumStorageDir("SignaturePad"),
-            "Signature_" + System.currentTimeMillis() + ".svg"
-        )
-        val stream: OutputStream = FileOutputStream(svgFile)
-        val writer = OutputStreamWriter(stream)
-        writer.write(signatureSvg)
-        writer.close()
-        stream.flush()
-        stream.close()
-        scanMediaFile(svgFile)
-        result = true
-
-        return result
-    }
-
-    companion object {
-        private const val REQUEST_EXTERNAL_STORAGE = 1
-        private val PERMISSIONS_STORAGE = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-
-        /**
-         * Checks if the app has permission to write to device storage
-         *
-         *
-         * If the app does not has permission then the user will be prompted to grant permissions
-         *
-         * @param activity the activity from which permissions are checked
-         */
-        fun verifyStoragePermissions(activity: Activity) {
-            // Check if we have write permission
-            val permission = ActivityCompat.checkSelfPermission(
-                activity,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-            )
-            if (permission != PackageManager.PERMISSION_GRANTED) {
-                // We don't have permission so prompt the user
-                ActivityCompat.requestPermissions(
-                    activity,
-                    PERMISSIONS_STORAGE,
-                    REQUEST_EXTERNAL_STORAGE
+            val signatureSvg = mSignaturePad.signatureSvg
+            val transparentSignatureBitmap = mSignaturePad.transparentSignatureBitmap
+            if (BuildConfig.DEBUG) {
+                Log.d("ViewActivity", "Bitmap size: " + signatureBitmap.byteCount)
+                Log.d(
+                    "ViewActivity",
+                    "Bitmap trasparent size: " + transparentSignatureBitmap.byteCount
                 )
+                Log.d("ViewActivity", "Svg length: " + signatureSvg.length)
             }
         }
     }
