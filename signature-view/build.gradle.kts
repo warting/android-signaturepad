18ed52bf484112f5cb3349abcab02442482d6e71
Stefan Wärting <stefan@warting.se>
2023-03-24 16:11:17 +0100

    bump kotlin compiler extension

--- signature-view/build.gradle.kts
+++ signature-view/build.gradle.kts
@@ -53,7 +53,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.3.2"
+        kotlinCompilerExtensionVersion = "1.4.4"
     }
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
