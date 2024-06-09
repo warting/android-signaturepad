c4f91c51e145a0214c709ba60ae95761d34e2d1a
Stefan Wärting <stefan@warting.se>
2024-06-09 10:01:27 +0200

    Bump all version 2024-06 (#319)

--- signature-view/build.gradle.kts
+++ signature-view/build.gradle.kts
@@ -1,22 +1,63 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     id("com.android.library")
-    id("kotlin-android")
+    alias(libs.plugins.kotlin.android)
     id("maven-publish")
     id("signing")
     id("org.jetbrains.dokka") version "1.8.20"
     id("com.gladed.androidgitversion") version "0.4.14"
     id("kotlin-kapt")
+    alias(libs.plugins.io.gitlab.arturbosch.detekt)
+    alias(libs.plugins.com.vanniktech.maven.publish)
+}
+
+mavenPublishing {
+
+    publishToMavenCentral(SonatypeHost.DEFAULT)
+    signAllPublications()
+
+    pom {
+        name.set("Signature - View")
+        description.set("Android Signature Pad is an Android library for drawing smooth signatures")
+        inceptionYear.set("2021")
+        url.set("https://github.com/warting/android-signaturepad/")
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://opensource.org/licenses/MIT")
+                distribution.set("https://opensource.org/licenses/MIT")
+            }
+        }
+        developers {
+            developer {
+                id.set("warting")
+                name.set("Stefan Wärting")
+                url.set("https://github.com/warting/")
+            }
+        }
+        scm {
+            url.set("https://github.com/warting/android-signaturepad/")
+            connection.set("scm:git:git://github.com/warting/android-signaturepad.git")
+            developerConnection.set("scm:git:ssh://git@github.com/warting/android-signaturepad.git")
+        }
+    }
+}
+
+detekt {
+    autoCorrect = true
+    buildUponDefaultConfig = true
 }
 
 androidGitVersion {
     tagPattern = "^v[0-9]+.*"
 }
 
-val PUBLISH_GROUP_ID: String by extra("se.warting.signature")
-val PUBLISH_VERSION: String by extra(androidGitVersion.name().replace("v", ""))
-val PUBLISH_ARTIFACT_ID by extra("signature-view")
+val PUBLISH_GROUP_ID: String by extra(rootProject.group as String)
+val PUBLISH_VERSION: String by extra(rootProject.version as String)
 
-apply(from = "${rootProject.projectDir}/gradle/publish-module.gradle")
+group = PUBLISH_GROUP_ID
+version = PUBLISH_VERSION
 
 android {
     compileSdk = 34
@@ -53,9 +94,6 @@ android {
         enable = true
     }
 
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.4"
-    }
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_17.toString()
         freeCompilerArgs = listOfNotNull(
@@ -86,5 +124,6 @@ java {
 
 dependencies {
     api(project(":signature-core"))
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation(libs.androidx.core.core.ktx)
+    detektPlugins(libs.io.gitlab.arturbosch.detekt.detekt.formatting)
 }
